035573c2a7cdc4da34ba6462cce6564c40f5437f
gpdzvapatsva <gpdzvapatsva@gmail.com>
2025-10-17 16:11:31 +0100

    initial upload

---
+++ products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+import datetime
+
+
+# Create your models here.
+class products(models.Model):
+    name = models.CharField(max_length=20)
+    product_description = models.CharField(max_length=20, null=True)
+    product_id = models.CharField(max_length=20)
+    quantity = models.CharField(max_length=20)
+    cost = models.DecimalField(max_digits=6, decimal_places=2, max_length=8)
+    prod_image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return f'{self.name},{self.product_type}, {self.product_id}, {self.quantity},{self.quantity}'
+
+    class Meta:
+        verbose_name_plural = 'Products'
